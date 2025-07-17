979f34d7b0a144b478d50e86fb580fde879d8512
brenda-barnaba <brendabarnaba06@gmail.com>
2025-07-17 15:31:55 +0300

    Update logout.php

--- authentication/logout.php
+++ authentication/logout.php
@@ -1,6 +1,34 @@
 <?php
 session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Delete the session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(), 
+        '', 
+        time() - 42000,
+        $params["path"], 
+        $params["domain"],
+        $params["secure"], 
+        $params["httponly"]
+    );
+}
+
 session_destroy();
+
+// Clear remember me token if exists
+if (isset($_COOKIE['remember_token'])) {
+    setcookie('remember_token', '', time() - 3600, "/");
+}
+
+// Prevent caching of protected pages
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
+
 header("Location: indexV51.php");
 exit();
 ?>
