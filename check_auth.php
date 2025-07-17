a04bab9650d52463eb2be8e17916925b68924518
Marya-Kahaly <marykahaly@gmail.com>
2025-07-17 13:50:15 +0300

    Update check_auth.php

--- check_auth.php
+++ check_auth.php
@@ -4,16 +4,16 @@ session_start();
 if (!isset($_SESSION['user_id'])) {
     // Check for remember me token
     if (isset($_COOKIE['remember_token'])) {
-        $conn = new mysqli("localhost", "", "root", "project");
+        $conn = new mysqli("localhost", "root", "", "project");
         
-        $stmt = $conn->prepare("SELECT id, full_name FROM users WHERE remember_token = ?");
+        $stmt = $conn->prepare("SELECT user_id, full_name FROM users WHERE remember_token = ?");
         $stmt->bind_param("s", $_COOKIE['remember_token']);
         $stmt->execute();
         $result = $stmt->get_result();
         
         if ($result->num_rows === 1) {
             $user = $result->fetch_assoc();
-            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['user_id'] = $user['user_id'];
             $_SESSION['user_name'] = $user['full_name'];
         }
         
